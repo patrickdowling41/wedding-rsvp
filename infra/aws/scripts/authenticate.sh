5866d75d947065b9b257ca7f087d82ccd31733a9
pdowling <pdowling@officeworks.com.au>
2024-12-19 14:31:41 +1100

    refactor: setup infra using sceptre + localstack

--- infra/aws/scripts/authenticate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-
-aws sso login
-temp_credentials=$(aws sts get-caller-identity --profile=default --query 'Credentials' --output text)
-
-export AWS_ACCESS_KEY_ID=$(echo $temp_credentials | cut -f 1)
-export AWS_SECRET_ACCESS_KEY=$(echo $temp_credentials | cut -f 2)
-export AWS_SESSION_TOKEN=$(echo $temp_credentials | cut -f 3)
